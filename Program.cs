b82cd5c4ba212cc970b99cbaf22ca87dcfa6bd41
connor gammill <connor.gammill@outlook.com>
2024-11-19 14:52:06 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Assignment_3._2._5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+			int[] numbers = { 1, 5, 3 };
+            int[] numbers2 = { 9, 8, 3 };
+            int[] numbers3 = { 1, 2, 3 };
+            int search = 5;
+            int search2 = 3;
+            int search3 = 4;
+            int index = Array.IndexOf(numbers, search); 
+            int index2 = Array.IndexOf(numbers2, search2);
+            int index3 = Array.IndexOf(numbers3, search3);
+
+			foreach (int number in numbers) 
+			{ 
+				Console.Write(number + " "); 
+			}
+			Console.WriteLine($"\nThe index of {search} is: {index}");
+
+			foreach (int number2 in numbers2)
+			{
+				Console.Write(number2 + " ");
+			}
+			Console.WriteLine($"\nThe index of {search2} is: {index2}");
+
+			foreach (int number3 in numbers3)
+			{
+				Console.Write(number3 + " ");
+			}
+			Console.WriteLine($"\nThe index of {search3} is: {index3}");
+		}
+    }
+}
